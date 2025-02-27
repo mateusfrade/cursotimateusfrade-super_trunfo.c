a78065d8def3480a84b4acca3be20c773c48ca02
mateusfrade <mateus.frade@hotmail.com>
2025-02-27 02:48:09 +0000

    acrencentando mais dois dados na carta do jogo

--- cartasuper_trunfo/CARTA_A01_BELO HORIZONTE.C.c
+++ cartasuper_trunfo/CARTA_A01_BELO HORIZONTE.C.c
@@ -9,15 +9,18 @@ int main (){
     float km = 331.4;
     float PIB = 105.82; 
     int quantidade_de_pontos_turisticos = 15;
+    float dencidade_populacional = (float) populacao / km;
+    float PIB_per_capita = (float) PIB / populacao;
 
     printf("CARTA 01\n");
     printf("Estado: %c\n", estado);
     printf("Codigo: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
+    printf("População: %d milhões \n", populacao);
     printf("Area: %f km² \n", km);
     printf("PIB: %f bilhões \n", PIB);
     printf("Quantidade de pontos turisticos: %d\n", quantidade_de_pontos_turisticos);
-
+    printf("Dencidade populacional: %.2f hab/km²\n", dencidade_populacional);
+    printf("PIB per Capita: %.2f reais \n", PIB_per_capita);
 
 }
--- cartasuper_trunfo/CARTA_B01_MANAUS.C
+++ cartasuper_trunfo/CARTA_B01_MANAUS.C
@@ -9,15 +9,19 @@ int main(){
     float km = 114.01;
     float PIB =   1032.81 ; 
     int quantidade_de_pontos_turisticos = 9;
+    float dencidade_populacional = (float) populacao / km;
+    float PIB_per_capita = (float) PIB / populacao;
 
     printf("CARTA 02\n");
     printf("Estado: %c\n", estado);
     printf("Codigo: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
+    printf("População: %d milhões \n", populacao);
     printf("Area: %f Km²\n", km);
     printf("PIB: %f bilhões\n", PIB);
     printf("Quantidade de pontos turisticos: %d\n", quantidade_de_pontos_turisticos);
-  
+  printf("Dencidade populacional: %.2f hab/km²\n", dencidade_populacional);
+    printf("PIB per Capita: %.2f reais \n", PIB_per_capita);
+
 
 }
